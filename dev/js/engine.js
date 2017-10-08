6e69bead37bcb475d58d8a2d0b228eb4814f531b
GrzegorzZapala <zapala.grzegorz@gmail.com>
2017-10-08 15:15:42 +0200

    Game as a separate Class

--- dev/js/engine.js
+++ dev/js/engine.js
@@ -16,161 +16,9 @@ document.addEventListener('DOMContentLoaded', function () {
     'use strict';
 
     // obliczenia ruchu komputera zawarto w computer
-    let computer = new Computer;
 
-    // globalna zmienna przechowujaca ustawienia gry
-    // przerobić jako Class
-    let gameReal = {
-        player: 0,
-        computer: 0,
-        current: 0,
-        opponent: 1,
-        activeTurn: 0,
-        notActive: 0,
-        board: [
-            [2, 2, 2],
-            [2, 2, 2],
-            [2, 2, 2]
-        ],
-        result: 0,
-        elements: {
-            // @type Element
-            finalBox: document.querySelector('#finalBox'),
-            modalBox: document.body.querySelector('.modal'),
-            initTextBox: document.querySelector('#initBox'),
-            finalBox__result: document.querySelector('#finalBox__result'),
-            finalBox__tryAgain: document.querySelector('#finalBox__tryAgain'),
-            // znak do wyboru przez gracza
-            playerCircle: document.querySelector('#playerCircle'),
-            playerCross: document.querySelector('#playerCross'),
-            boardFields: document.querySelectorAll('.board__field')
-            
-        },
-
-        /**
-         * ustawia wartość gracza oraz komputera 
-         * @param {number} choice
-         */
-        setPlayer: function (choice) {
-            this.player = choice;
-            this.computer = (this.player === 1) ? 0 : 1;
-            
-            this.animateInitBox();
-            if (this.player === 1) {
-                this.current = this.computer;
-                this.opponent = this.player;
-                // TODO a może setField(komputer/gracz, wartość computer.minimax(gameReal);
-                gameReal.setBoardField(computer.setFirstMove());
-            }
-        },
-        
-        /**
-         * Określa pole gracza na planszy 
-         * @param {(object | number[])} data object to data
-         */
-        setBoardField: function (data) {
-            let result;
-            let boardField;
-            let _this = this;
-            /**
-             * Sprawdza czy ruch gracza jest prawidłowy
-             * @param {object} chosenField to jest object, property MouseEvent.target pochodzący z event'u 'click' Player'a
-             * @return {boolean}
-             */
-            function isPlayerValidMove (chosenField) {
-                // czy zostało wybrane pole gry i czy jest nadal wolne
-                if (chosenField.dataset.field !== undefined && _this.board[boardField.dataset.field[0]][boardField.dataset.field[1]] === 2) {
-                    return true;
-                }
-            }
-            /**
-             * Zaznacza wybrane pole w DOM
-             * @param {object} field HTMLelement 
-             */
-            function setBoardFieldDOM (field) {
-                if (_this.current === 1) {
-                    field.innerHTML = '<svg class="board__icon board__icon--shapes"><use xlink:href="#shapes"/></svg>';
-                } else {
-                    field.innerHTML = '<svg class="board__icon board__icon--circle"><use xlink:href="#circle"/></svg>';
-                }
-            } 
-            if (Array.isArray(data)) {
-
-                // computer
-                this.board[data[0]][data[1]] = this.current;
-                let boardField = document.querySelector('[data-field="' + data[0] + data[1] + '"]');
+    let gameReal = new Game();
     
-                setBoardFieldDOM (boardField);
-                result = Computer.setScore(gameReal);
-                if (result !== undefined) {
-                    this.prepareFinalBox(result);
-                }
-                this.current = this.player;
-            } else {
-
-                // player
-                boardField = data.target;
-                let row;
-                let column;
-        
-                if (isPlayerValidMove(boardField)) {
-                    row = boardField.dataset.field[0];
-                    column = boardField.dataset.field[1];
-
-                    this.board[row][column] = this.current;
-                    setBoardFieldDOM (boardField);
-        
-                    // TODO wyciagnąć jako funkcję, jeśli racjonalne
-                    result = Computer.setScore(gameReal);
-                    if (result === undefined) {
-                        computer.setMove(gameReal);
-                        // this.current = this.computer;
-                        // this.opponent = this.player;
-                        
-                        // // TODO sprawdzić co się dzieje w minimaxie z activeTurn
-                        // this.activeTurn = this.current;
-                        // computer.minimax(gameReal);
-                        // gameReal.setBoardField(computer.choice);
-                    } else {
-                        // FIXME funkcja końcowa
-                        this.prepareFinalBox(result);
-                    }
-                }
-            }
-        },
-        /**
-         * wywołuje animację dla inicjalnego dialogu
-         */
-        animateInitBox: function () {
-            this.elements.initTextBox.classList.toggle('visible');
-            // chowanie modalu po animacji
-            setTimeout(() => this.elements.modalBox.style.display = 'none', 1500);
-        },
-
-        /**
-         * TODO już czuć elementy wspólne
-         * wywołuje animację dla inicjalnego dialogu
-         * @param {number} time - czas do uruchomienia końcowej animacji 
-         */
-        animateFinalBox: function (time) {
-            this.elements.modalBox.style.display = 'block';
-            // gameReal.elements.finalBox.style.transform = 'rotateY(89deg)';
-            setTimeout( function () {
-                gameReal.elements.finalBox.classList.toggle('visible');
-            }, time);
-        },
-        
-        /**
-         * 
-         * @param {number} result 
-         */
-        prepareFinalBox: function (result) {
-            const winner = result ? 'computer wins' : 'draw';
-            this.elements.finalBox__result.textContent = winner;
-            this.animateFinalBox();
-        }
-    };
-
 
     function replay () {
         gameReal.board = [
@@ -182,16 +30,16 @@ document.addEventListener('DOMContentLoaded', function () {
         gameReal.opponent = 0;
         gameReal.current = 0;
         gameReal.result = 0;
-        gameReal.elements.boardFields.forEach((value)=> {
+        gameReal.elements.boardFields.forEach((value) => {
             value.innerHTML = '';
         });
 
         gameReal.elements.finalBox.classList.toggle('visible');
-        setTimeout( () =>  gameReal.elements.initTextBox.classList.toggle('visible'), 1500 );
+        setTimeout(() => gameReal.elements.initTextBox.classList.toggle('visible'), 1500);
 
     }
 
-    
+
     /**
      *  inicjalizacja gry
      */
---
+++ dev/js/game.js
@@ -0,0 +1,143 @@
+class Game {
+    constructor () {
+        this.player = 0;
+        this.computer = 0;
+        this.current = 0;
+        this.opponent = 1;
+        this.activeTurn = 0;
+        this.notActive = 0;
+        this.board = [
+            [2, 2, 2],
+            [2, 2, 2],
+            [2, 2, 2]
+        ];
+        this.result = 0;
+        this.elements = {
+
+            finalBox: document.querySelector('#finalBox'),
+            modalBox: document.body.querySelector('.modal'),
+            initTextBox: document.querySelector('#initBox'),
+            finalBox__result: document.querySelector('#finalBox__result'),
+            finalBox__tryAgain: document.querySelector('#finalBox__tryAgain'),
+            playerCircle: document.querySelector('#playerCircle'),
+            playerCross: document.querySelector('#playerCross'),
+            boardFields: document.querySelectorAll('.board__field')
+
+        };
+        this.computerEngine = new Computer();
+    }
+
+    /**
+     * Określa pole gracza na planszy 
+     * @param {object} data HTMLelement
+     */
+    setBoardField (data) {
+        let _this = this;
+        let boardField;
+        let result;
+        let row;
+        let column;
+
+        /**
+         * Sprawdza czy ruch gracza jest prawidłowy
+         * @param {object} chosenField to jest object, property MouseEvent.target pochodzący z event'u 'click' Player'a
+         * @return {boolean}
+         */
+        function isPlayerValidMove (chosenField) {
+
+            // czy zostało wybrane pole gry i czy jest nadal wolne
+            if (chosenField.field !== undefined && _this.board[chosenField.field[0]][chosenField.field[1]] === 2) {
+                row = chosenField.field[0];
+                column = chosenField.field[1];
+                return true;
+            }
+        }
+
+        /**
+         * Zaznacza wybrane pole w DOM
+         * @param {object} field HTMLelement 
+         */
+        function setBoardFieldDOM (field) {
+            if (_this.current === 1) {
+                field.innerHTML = '<svg class="board__icon board__icon--shapes"><use xlink:href="#shapes"/></svg>';
+            } else {
+                field.innerHTML = '<svg class="board__icon board__icon--circle"><use xlink:href="#circle"/></svg>';
+            }
+        }
+
+        if (data.target === undefined) {
+
+            // computer
+            row = data.dataset.field[0];
+            column = data.dataset.field[1];
+
+            // zaznaczenie na planszy wewnętrznej
+            this.board[row][column] = this.current;
+
+            // zaznaczenie na planszy DOM
+            setBoardFieldDOM(data);
+            result = Computer.getScore(this);
+            if (result !== undefined) {
+                this.prepareFinalBox(result);
+            }
+            this.current = this.player;
+        } else {
+
+            // player
+            boardField = data.target;
+
+            if (isPlayerValidMove(boardField.dataset)) {
+
+                this.board[row][column] = this.current;
+                setBoardFieldDOM(boardField);
+
+                result = Computer.getScore(this);
+                if (result !== undefined) {
+                    this.prepareFinalBox(result);
+                } else {
+                    this.computerEngine.setMove(this);
+                }
+            }
+        }
+    }
+
+    /**
+     * ustawia wartość gracza oraz komputera 
+     * @param {number} choice
+     */
+    setPlayer (choice) {
+        this.player = choice;
+        this.computer = (this.player === 1) ? 0 : 1;
+
+        this.animateInitBox();
+        if (this.player === 1) {
+            this.current = this.computer;
+            this.opponent = this.player;
+            this.setBoardField(this.computerEngine.setFirstMove());
+        }
+    }
+
+    /**
+     * wywołuje animację dla inicjalnego dialogu
+     */
+    animateInitBox () {
+        this.elements.initTextBox.classList.toggle('visible');
+        // chowanie modalu po animacji
+        setTimeout(() => this.elements.modalBox.style.display = 'none', 1500);
+    }
+
+    /**
+     * Wywołuje animację dla końcowego dialogu
+     * @param {number} result 
+     */
+    prepareFinalBox (result) {
+        const winner = result ? 'computer wins' : 'draw';
+        this.elements.finalBox__result.textContent = winner;
+        this.elements.modalBox.style.display = 'block';
+        
+        // zwykła funkcja tworzy scope w którym this jest undefined; arrow function przekazuje scope
+        setTimeout( () => {
+            this.elements.finalBox.classList.toggle('visible');
+        }, 0);
+    }
+}
