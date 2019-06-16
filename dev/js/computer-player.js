a7ce6ebcab77fba0199abc50636d2dda993b74b7
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2019-06-16 11:53:08 +0200

    refreshing

--- dev/js/computer-player.js
+++ dev/js/computer-player.js
@@ -1,7 +1,7 @@
 /**
- * Klasa odpowiadająca za logikę komputera 
+ * Klasa odpowiadająca za logikę komputera
  */
-// export 
+// export
 class Computer {
     constructor () {
         // kluczowa tablica przechowująca 'najlepszy' ruch
@@ -12,10 +12,10 @@ class Computer {
      * Zwraca wartość liczbową określającą stan gry:
      * 1 - wygrał gracz, '-1' - wygrał komputer, 0 - remis
      * undefined - gdy gra się nie zakończyła
-     * 
-     *  Przyjmuje obiekt game przechowujący informacje o grze 
      *
-     * @param {object} game 
+     *  Przyjmuje obiekt game przechowujący informacje o grze
+     *
+     * @param {object} game
      * @return {number} The x value.
      */
     static getScore (game) {
@@ -42,7 +42,7 @@ class Computer {
             return game.board[currentValue] === game.opponent;
         }
 
-        // sprawdź wartości na tablicy 
+        // sprawdź wartości na tablicy
         for (let i = 0, len = winCombos.length; i < len; i++) {
             if (winCombos[i].every(currentWin)) {
                 return 1;
@@ -51,20 +51,20 @@ class Computer {
             }
         }
         // check for draw
-        draw = game.board.every( (value, index) => {
-            return game.board[index] !== 2;   
-        }); 
+        draw = game.board.every((value, index) => {
+            return game.board[index] !== 2;
+        });
         if (draw) {
-            return 0;  
+            return 0;
         }
         return undefined;
     }
 
     /**
      * Zwraca tablicę dopuszczalnych ruchów
-     * 
-     * @param {number[]} game 
-     * @returns {number[]} 
+     *
+     * @param {number[]} game
+     * @returns {number[]}
      */
     getAvailableMoves (board) {
         let availableMoves = [];
@@ -76,9 +76,9 @@ class Computer {
 
     /**
      * Funkcja pomocnicza dla algorytmu minimax
-     * Generuje wariant stanu gry 
-     * @param {number} move 
-     * @param {Object} game 
+     * Generuje wariant stanu gry
+     * @param {number} move
+     * @param {Object} game
      */
     getNewState (move, game) {
         // kopiowanie tworzy referencję
@@ -90,47 +90,44 @@ class Computer {
 
         newGame.board[move] = newGame.activeTurn;
 
-        newGame.activeTurn = (newGame.activeTurn === 1) ? 0 : 1;
+        newGame.activeTurn = newGame.activeTurn === 1 ? 0 : 1;
 
         return newGame;
     }
 
     /**
-     * 
+     *
      * Wyznacza ruch komputera na podstawie algorytmu minimax
      * Wynik jest zapisywany we właściwości choice
      * @param {object} game
      * @return {number} The x value.
      */
     minimax (game) {
-        
         // return score if game over
         let score = Computer.getScore(game);
         if (score !== undefined) return score;
-        
+
         let scores = [];
         let moves = [];
-        
+
         // Populate the scores indexay, recursing as needed
         this.getAvailableMoves(game.board).forEach(function iterateMoves (move) {
-        
             let possibleGame = this.getNewState(move, game);
-        
+
             scores.push(this.minimax(possibleGame));
-        
+
             moves.push(move);
         }, this);
-        
+
         // Do the min or the max calculation
         // This is the max calculation
-        // 
+        //
         if (game.activeTurn === game.current) {
             let max_score_index = scores.indexOf(Math.max(...scores));
-        
+
             this.choice = moves[max_score_index];
             return scores[max_score_index];
         } else {
-        
             // This is the min calculation
             let min_score_index = scores.indexOf(Math.min(...scores));
             this.choice = moves[min_score_index];
@@ -146,21 +143,23 @@ class Computer {
      */
     setFirstMove () {
         let possibleMoves = [0, 2, 6, 8];
-        let choice = Math.floor((Math.random() * 10 % 4));
+        let choice = Math.floor((Math.random() * 10) % 4);
         return possibleMoves[choice];
     }
 
     /**
      * Perform computer move
-     * @param {object} game 
+     * @param {object} game
      */
     setMove (game) {
         game.current = game.computer;
         game.opponent = game.player;
         game.activeTurn = game.current;
 
-
         this.minimax(game);
-        game.setBoardField(this.choice, 'computer');
+        /* sztuczne opóźnienie, aby komputer "udawał, że pracuje nad odpowiedzią" */
+        setTimeout(() => {
+            game.setBoardField(this.choice, 'computer');
+        }, 1000);
     }
 }
